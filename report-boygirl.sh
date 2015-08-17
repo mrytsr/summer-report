f207bc4d29f29b9add58c4e768b29b7e711d9f5e
mrytsr <mrytsr@qq.com>
2015-08-17 12:03:17 +0800

    add -A

--- report-boygirl.sh
+++ report-boygirl.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 awk  -F'" "|"$' '
 BEGIN{
-    print "男比例|男|女|故事"
-    print "---|---|---|---"
 }{
     if($5!="NULL"){
         if($5==1){
@@ -13,12 +11,14 @@ BEGIN{
         taxo[$6]=$7
     }
 }END{
+    # print "男比例|男|女|故事"
+    # print "---|---|---|---"
     for(i in boy){
         if(boy[i]<20&&girl[i]<20)
             continue;
         boymark = boy[i] / (boy[i] + girl[i])
         if(girl[i]==""||boy[i]/girl[i]>1.4||boy[i]==""||girl[i]/boy[i]>1.4){
-            print  boymark " | " boy[i]" | "girl[i]" | "  i | "sort -r -n -k1";
+            # print  boymark " | " boy[i]" | "girl[i]" | "  i | "sort -r -n -k1";
             split(taxo[i], ts, ",")
             if(boymark > 0.5){
                 for(t in ts){
@@ -33,13 +33,15 @@ BEGIN{
     }
 }END{
     print "taxo | taxogirl"
+    print "---|---"
     for(t in taxogirl){
-        if(taxogirl[t])
+        if(taxogirl[t] > 8)
             print taxogirl[t] " | " t | "sort -rnk1"
     }
     # print "taxo | taxoboy"
+    # print "---|---"
     # for(t in taxoboy){
-    #     if(taxoboy[t])
+    #     if(taxoboy[t] > 8)
     #         print taxoboy[t] " | " t | "sort -rnk1"
     # }
 }' event_3.index >> report-boygirl.md
