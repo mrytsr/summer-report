903adc6d635483429acc2e282002fe1be00891e4
mrytsr <mrytsr@qq.com>
2015-08-17 10:50:36 +0800

    add -A

--- report-boygirl.sh
+++ report-boygirl.sh
@@ -13,12 +13,9 @@ BEGIN{
     }
 }END{
     for(i in boy){
-        mark[i] = boy[i] / (boy[i] + girl[i])
-    }
-    for(i in mark){
         if(boy[i]<20&&girl[i]<20)
             continue;
         if(girl[i]==""||boy[i]/girl[i]>1.4||boy[i]==""||girl[i]/boy[i]>1.4)
-            print boy[i]" | "girl[i]" | " mark[i]  " | " i;
+            print boy[i] / (boy[i] + girl[i])  " | " boy[i]" | "girl[i]" | "  i | "sort -r -n -k1";
     }
 }' event_3.index > report-boygirl.md
