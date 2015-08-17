5722f8230cc4dcab874669f043e5d71ccbfdd347
mrytsr <mrytsr@qq.com>
2015-08-17 10:09:18 +0800

    add -A

---
+++ report-boygirl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+awk  -F'" "' '
+{
+    if($5!="NULL"){
+        if($5==1){
+            boy[$6" - "$7]++;
+        }else{
+            girl[$6" - "$7]++;
+        }
+    }
+}END{
+    for(i in boy){
+        if(boy[i]<10&&girl[i]<10)
+            continue;
+        if(girl[i]==""||boy[i]/girl[i]>1.2||boy[i]==""||girl[i]/boy[i]>1.2)
+            print boy[i]" | "girl[i]" | "i;
+    }
+}' event_3.index > report-boygirl.md
